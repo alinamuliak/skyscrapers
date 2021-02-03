9598ee8d8e299d14e7b3d1130759b3c58eb8f41b
Alina Muliak <alina.muliak@ucu.edu.ua>
2021-02-03 13:43:29 +0200

    Finish two first functions

--- skyscrapers.py
+++ skyscrapers.py
@@ -1,15 +1,19 @@
-def read_input(path: str):
+def read_input(path: str) -> list:
     """
     Read game board file from path.
     Return list of str.
 
     >>> read_input("check.txt")
-    ['***21**', '452453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***']
+    ['***21**', '412453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***']
     """
-    pass
+    list_of_lines = []
+    with open(path, 'r') as f:
+        for line in f.readlines():
+            list_of_lines.append(line.strip())
+    return list_of_lines
 
 
-def left_to_right_check(input_line: str, pivot: int):
+def left_to_right_check(input_line: str, pivot: int) -> bool:
     """
     Check row-wise visibility from left to right.
     Return True if number of building from the left-most hint is visible looking to the right,
@@ -23,7 +27,10 @@ def left_to_right_check(input_line: str, pivot: int):
     >>> left_to_right_check("452453*", 5)
     False
     """
-    pass
+    max_building = max(list(input_line[:pivot]))
+    if max_building <= input_line[pivot]:
+        return True
+    return False
 
 
 def check_not_finished_board(board: list):
